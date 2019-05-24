b4ffc4500c54d39a888dd2d4c9d95762e89937ea
wadie <wadiesoyass@gmail.com>
2019-05-24 20:55:57 +0200

    maintenance==> CRUD =Ok Tuning mise en forme => ok

--- src/app/services/tuning/gestion-tuning.service.ts
+++ src/app/services/tuning/gestion-tuning.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,16 @@ export class GestionTuningService {
 
   getAllTuning() {
     return this.http.get(this.host + '/getAllTuning');
+  }
+
+  //observable c'est pour attendre le fichier se telecharger (synchron)//
+  upLoadData(file: File, id): Observable<HttpEvent<{}>> {
+    let formdata: FormData = new FormData();
+    formdata.append('file', file);
+    const req = new HttpRequest('POST', this.host + '/uploadPhoto/' + id, formdata, {
+      reportProgress: true,
+      responseType: 'text'
+    });
+    return this.http.request(req);
   }
 }
--- src/app/tuning/tuning.component.ts
+++ src/app/tuning/tuning.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {GestionTuningService} from '../services/tuning/gestion-tuning.service';
+import {HttpEventType, HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-tuning',
@@ -9,11 +10,17 @@ import {GestionTuningService} from '../services/tuning/gestion-tuning.service';
 export class TuningComponent implements OnInit {
   tunings;
   i = 0;
+  private editPhoto: boolean;
+  private currentPhoto;
+  private selectedFilles;
+  private progresse;
+  private currentFileUpload;
 
   constructor(private tuningService: GestionTuningService) {
   }
 
   ngOnInit() {
+    this.i = 0;
     this.onGetAllTuning();
   }
 
@@ -33,5 +40,28 @@ export class TuningComponent implements OnInit {
       this.i--;
       return this.i;
     }
+  }
+
+  onEditPhoto(data) {
+    this.editPhoto = true;
+    this.currentPhoto = data;
+  }
+
+  onSelectedFile(event) {
+    this.selectedFilles = event.target.files;
+  }
+
+  uploadPhoto() {
+    this.progresse = 0;
+    this.currentFileUpload = this.selectedFilles.item(0);
+    this.tuningService.upLoadData(this.currentFileUpload, this.currentPhoto.id).subscribe(event => {
+      if (event.type == HttpEventType.UploadProgress) {
+        this.progresse = Math.round(100 * event.loaded / event.total);
+      } else if (event instanceof HttpResponse) {
+        alert('données sont chargés avec succés');
+      }
+    }, err => {
+      alert("error de chargement de l'image, merci d'essayer");
+    });
   }
 }
