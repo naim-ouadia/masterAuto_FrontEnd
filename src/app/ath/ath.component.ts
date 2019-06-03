9540baeb6fb117c00ca49a59e439c79b28163dde
wadie <wadiesoyass@gmail.com>
2019-06-04 00:35:53 +0200

    ATH==> mise en forme ok

--- src/app/ath/ath.component.ts
+++ src/app/ath/ath.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthentificationService} from '../services/client/authentification.service';
 import {Router} from '@angular/router';
 import {GestionATHService} from '../services/ATH/gestion-ath.service';
+import {GestionContactService} from '../services/contact/gestion-contact.service';
 
 @Component({
   selector: 'app-ath',
@@ -18,12 +19,16 @@ export class ATHComponent implements OnInit {
   private fichiers;
   private fichier;
   private nbr: string;
+  private nbr2: string;
+  private contacts;
 
-  constructor(private authService: AuthentificationService, private router: Router, private athService: GestionATHService) {
+  constructor(private authService: AuthentificationService, private router: Router, private athService: GestionATHService, private contactService: GestionContactService) {
   }
 
   ngOnInit() {
+    this.nbr = 'A';
     this.authenticated = this.authService.isAuthenticated();
+    this.onGetAllContact();
   }
 
   onAuthenticated() {
@@ -34,7 +39,6 @@ export class ATHComponent implements OnInit {
     this.athService.AllAth().subscribe(data => {
       this.aths = data;
       this.nbr = 'A';
-      console.log('' + this.nbr);
     }, err => {
       console.log('error de chargement de liste Ath');
     });
@@ -44,7 +48,6 @@ export class ATHComponent implements OnInit {
     this.athService.AllDepnnage().subscribe(data => {
       this.nbr = 'B';
       this.depannages = data;
-      console.log('' + this.nbr);
     }, err => {
       console.log('error de chargement de liste Ath');
     });
@@ -54,7 +57,6 @@ export class ATHComponent implements OnInit {
     this.athService.AllFichier().subscribe(data => {
       this.fichiers = data;
       this.nbr = 'C';
-      console.log('' + this.nbr);
     }, err => {
       console.log('error');
     });
@@ -63,6 +65,7 @@ export class ATHComponent implements OnInit {
   onGetAth(id) {
     this.athService.getAthById(id).subscribe(data => {
       this.athObj = data;
+      this.nbr2 = 'AA';
     }, err => {
       console.log('error de chargement athObj');
     });
@@ -71,6 +74,7 @@ export class ATHComponent implements OnInit {
   onGetDepannage(id) {
     this.athService.getDepanngeId(id).subscribe(data => {
       this.depannage = data;
+      this.nbr2 = 'BB';
     }, err => {
       console.log('error');
     });
@@ -80,8 +84,26 @@ export class ATHComponent implements OnInit {
   onGetFichier(id) {
     this.athService.getFichierById(id).subscribe(data => {
       this.fichier = data;
+      this.nbr2 = 'CC';
     }, err => {
       console.log('error de chargement de fichier');
+    });
+  }
+
+  onGetAllContact() {
+    this.contactService.getAllContact().subscribe(data => {
+      this.contacts = data;
+    }, err => {
+      console.log('error');
+    });
+  }
+
+  onRemoveFichier(id) {
+    console.log('test');
+    this.athService.removeFichier(id).subscribe(resp => {
+      this.onAllFichier();
+    }, err => {
+      console.log('error');
     });
   }
 }
